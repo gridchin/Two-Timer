9d863bf3105198cf61c3475983bc1189f159c03e
Denis <denis.fdez@gmail.com>
2014-05-15 12:32:56 +0200

    Passing transforms from CSS to JS

--- js/script.js
+++ js/script.js
@@ -5,8 +5,31 @@ jQuery(document).ready(function(){
 		var d = i + 1;
 		var dial = d * 6;
 		$('.dial').append('<li class="d' + d + '"></li>');
-		$('.dial .d' + d).css("transform", "rotate(" + dial + "deg)");		
+		$('.dial .d' + d).css('transform', 'rotate(' + dial + 'deg)');		
 	}
+	
+	for (i=45;i<50;i++){
+		var d = i + 1;
+		var n = i - 45;
+		var marginLeft = -172 - n*16;
+		$('.dial .d' + d).css('transform', 'rotate(270deg)');
+		$('.dial .d' + d).css('margin-left', marginLeft + 'px');
+	}
+	
+	for (i=50;i<55;i++){
+		var d = i + 1;
+		var n = i - 50;
+		var deg = 285 + n*15;
+		$('.dial .d' + d).css('transform', 'rotate(' + deg + 'deg)');
+	}
+	
+	for (i=55;i<61;i++){
+		var d = i + 1;
+		var n = i - 55;
+		var marginTop = 80 - n*16;
+		$('.dial .d' + d).css('transform', 'rotate(0)');
+		$('.dial .d' + d).css('margin-top', marginTop + 'px');
+	}	
 	
 	// Share
 	$('.share_btn').click(function () {
